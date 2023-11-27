ed7c35b600b103606741f12b8d900324c4aa8107
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-11-27 12:19:40 +0100

    Initial Commit

---
+++ Nogi Logger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nogi Logger
+//
+//  Created by Hossein Sharifi on 27/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nogi Logger/Nogi_LoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nogi_LoggerApp.swift
+//  Nogi Logger
+//
+//  Created by Hossein Sharifi on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Nogi_LoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
